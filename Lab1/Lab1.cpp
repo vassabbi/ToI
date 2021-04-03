5035bede7160f1eadb8580b3ed6b82064a0e53fd
vassabbi <vassabbi0@gmail.com>
2021-04-03 04:44:00 +0300

    Adding method of turning grille. Creating interface

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <Windows.h>
 
 using namespace std;
 
@@ -13,6 +15,10 @@ char const keyPlayfairMatrix[playfairSizeX][playfairSizeY] = {
 	{'D', 'E', 'F', 'I', 'K'},
 	{'L', 'M', 'N', 'Q', 'S'},
 	{'U', 'V', 'W', 'X', 'Z'}
+};
+
+pair<int, int> const keyRoatingLattice[4] = {
+	{0, 0}, {1, 3}, {2, 2}, {3, 1}
 };
 
 void changeIndent(int &numOfIndent) {
@@ -130,6 +136,65 @@ string decoderColumnarMethod(string codeWord, string key) {
 	return word;
 }
 
+string coderRotatingLatticeMethod(string word) {
+	string codeWord = "";
+	for (int i = 0; i < 16; i++) codeWord += "*";
+	int num = 0;
+	pair<int, int> keyRoatingLatticeChange[4];
+	for (int i = 0; i < 4; i++) {
+		keyRoatingLatticeChange[i].first = keyRoatingLattice[i].first;
+		keyRoatingLatticeChange[i].second = keyRoatingLattice[i].second;
+	}
+	for (int i = 0; i < word.length(); i++) {
+		int posX = keyRoatingLatticeChange[num].first;
+		int posY = keyRoatingLatticeChange[num].second;
+		if ((i != 0) && ((i % 4) == 0)) {
+			for (int j = 0; j < 4; j++) {
+				int oldPosX = keyRoatingLatticeChange[j].first;
+				int oldPosY = keyRoatingLatticeChange[j].second;
+				keyRoatingLatticeChange[j].first = oldPosY;
+				keyRoatingLatticeChange[j].second = 3 - oldPosX;
+			}
+			sort(keyRoatingLatticeChange, keyRoatingLatticeChange + 4);
+		}
+		codeWord[keyRoatingLatticeChange[num].first * 4 + keyRoatingLatticeChange[num].second] = word[i];
+		num++;
+		num = num % 4;
+	}
+	return codeWord;
+}
+
+string decoderRotatingLatticeMethod(string codeWord) {
+	string word = "";
+	for (int i = 0; i < 16; i++) word += "*";
+	int num = 0;
+	pair<int, int> keyRoatingLatticeChange[4];
+	for (int i = 0; i < 4; i++) {
+		keyRoatingLatticeChange[i].first = keyRoatingLattice[i].first;
+		keyRoatingLatticeChange[i].second = keyRoatingLattice[i].second;
+	}
+	for (int i = 0; i < codeWord.length(); i++) {
+		int posX = keyRoatingLatticeChange[num].first;
+		int posY = keyRoatingLatticeChange[num].second;
+		if ((i != 0) && ((i % 4) == 0)) {
+			for (int j = 0; j < 4; j++) {
+				int oldPosX = keyRoatingLatticeChange[j].first;
+				int oldPosY = keyRoatingLatticeChange[j].second;
+				keyRoatingLatticeChange[j].first = oldPosY;
+				keyRoatingLatticeChange[j].second = 3 - oldPosX;
+			}
+			sort(keyRoatingLatticeChange, keyRoatingLatticeChange + 4);
+		}
+		word[i] = codeWord[keyRoatingLatticeChange[num].first * 4 + keyRoatingLatticeChange[num].second];
+		num++;
+		num = num % 4;
+	}
+	while ((word.length() > 0) && (word[word.length() - 1]) == '*') {
+		word.erase(word.length() - 1, 1);
+	}
+	return word;
+}
+
 pair<int, int> findPosInPlayfairMatrix(char c) {
 	if (c == 'J') {
 		c = 'I';
@@ -222,7 +287,101 @@ string decoderPlayfair(string codeWord) {
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	int type = 0;
+	while (type != 9) {
+		cout << "Введите 1, если хотите выбрать метод железной изгороди\n";
+		cout << "Введите 2, если хотите выбрать столбцовый метод\n";
+		cout << "Введите 3, если хотите выбрать метод поворачивающейся решетки\n";
+		cout << "Введите 4, если хотите выбрать Шифр Плейфера\n";
+		cout << "Введите 9, если хотите выйти\n";
+		cin >> type;
+		int typeCD = 0;
+		switch (type) {
+		case 1:
+			while (typeCD != 9) {
+				cout << "Введите 1, если хотите использовать шифратор\n";
+				cout << "Ввелите 2, если хотите использовать дешифратор\n";
+				cout << "Введите 9, если хотите выйти\n";
+				cin >> typeCD;
+				if (typeCD != 9) {
+					cout << "Введите слово и ключ\n";
+					string word;
+					int key;
+					cin >> word >> key;
+					if (typeCD == 1) {
+						cout << "Ответ - " << coderRailFenceMethod(word, key) << '\n';
+					}
+					else {
+						cout << "Ответ - " << decoderRailFenceMethod(word, key) << '\n';
+					}
+				}
+			}
+			break;
+
+		case 2:
+			while (typeCD != 9) {
+				cout << "Введите 1, если хотите использовать шифратор\n";
+				cout << "Ввелите 2, если хотите использовать дешифратор\n";
+				cout << "Введите 9, если хотите выйти\n";
+				cin >> typeCD;
+				if (typeCD != 9) {
+					cout << "Введите слово и ключ\n";
+					string word;
+					string key;
+					cin >> word >> key;
+					if (typeCD == 1) {
+						cout << "Ответ - " << coderColumnarMethod(word, key) << '\n';
+					}
+					else {
+						cout << "Ответ - " << decoderColumnarMethod(word, key) << '\n';
+					}
+				}
+			}
+			break;
+
+		case 3:
+			while (typeCD != 9) {
+				cout << "Введите 1, если хотите использовать шифратор\n";
+				cout << "Ввелите 2, если хотите использовать дешифратор\n";
+				cout << "Введите 9, если хотите выйти\n";
+				cin >> typeCD;
+				if (typeCD != 9) {
+					cout << "Введите слово\n";
+					string word;
+					cin >> word;
+					if (typeCD == 1) {
+						cout << "Ответ - " << coderRotatingLatticeMethod(word) << '\n';
+					}
+					else {
+						cout << "Ответ - " << decoderRotatingLatticeMethod(word) << '\n';
+					}
+				}
+			}
+			break;
+		case 4:
+			while (typeCD != 9) {
+				cout << "Введите 1, если хотите использовать шифратор\n";
+				cout << "Ввелите 2, если хотите использовать дешифратор\n";
+				cout << "Введите 9, если хотите выйти\n";
+				cin >> typeCD;
+				if (typeCD != 9) {
+					cout << "Введите слово\n";
+					string word;
+					cin >> word;
+					if (typeCD == 1) {
+						cout << "Ответ - " << coderPlayfair(word) << '\n';
+					}
+					else {
+						cout << "Ответ - " << decoderPlayfair(word) << '\n';
+					}
+				}
+			}
+			break;
+		}
+	}
 	string s;
 	getline(cin, s);
-	cout << decoderPlayfair(s);
 }
